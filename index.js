5ba55d7b6e19a0d7e2090e8111b71f7ad6ede1b7
Hui Wang <huiw3939@gmail.com>
2018-04-27 15:37:55 -0400

    can load the map

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-import React, { Component } from 'react'
-import $S from 'scriptjs'
-import Gmap from './src/mapComponent'
+import React, { Component } from 'react';
+var $S = require('scriptjs');
 
 class SimpleGoogleMaps extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      mapLoaded: false
+    };
+  }
+
+  componentDidMount(){
+    window.google ? this.setState({
+      mapLoaded:true
+    }) :
+    $S(`https://maps.googleapis.com/maps/api/js?key=${this.props.apiKey}`, () => {
+      this.setState({
+        mapLoaded:true
+      })
+    });
+  }
+
   render(){
-    return(
-      <Gmap
-        apiKey={""}
-        height={"400px"}
-        width={"100%"}
-      }>
-    )
+    if(this.state.mapLoaded){
+      new window.google.maps.Map(document.getElementById('map'), {
+        zoom: this.props.zoom,
+        center: {
+          lat: this.props.center.lat,
+          lng: this.props.center.lng
+        }
+      });
+    }
+    return (
+      <div id="map" style={this.props.style}></div>
+    );
   }
 }
 
-export default SimpleGoogleMaps
+export default SimpleGoogleMaps;
--- src/loadGoogleMaps.js
+++
@@ -1,4 +0,0 @@
-import $S from 'scriptjs'
-const baseUrl = `https://maps.googleapis.com/maps/api/js?key=`;
-
-export default async (apiKey) => await ($S(`${baseUrl}${apiKey}`))
